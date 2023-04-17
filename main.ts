9cf2d72272d0095a4a3ab165ec363e8db81c704b
Gyat-Gabby <130679972+Gyat-Gabby@users.noreply.github.com>
2023-04-17 09:50:25 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,33 +26,44 @@ music.playTone(659, music.beat(BeatFraction.Half))
 music.playTone(523, music.beat(BeatFraction.Quarter))
 music.playTone(587, music.beat(BeatFraction.Quarter))
 music.playTone(494, music.beat(BeatFraction.Quarter))
-music.stopAllSounds()
-music.playTone(523, music.beat(BeatFraction.Half))
-music.playTone(392, music.beat(BeatFraction.Half))
-music.playTone(330, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(494, music.beat(BeatFraction.Half))
-music.playTone(466, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(392, music.beat(BeatFraction.Half))
-music.playTone(659, music.beat(BeatFraction.Half))
-music.playTone(784, music.beat(BeatFraction.Half))
-music.playTone(880, music.beat(BeatFraction.Half))
-music.playTone(698, music.beat(BeatFraction.Half))
-music.playTone(784, music.beat(BeatFraction.Half))
-music.playTone(659, music.beat(BeatFraction.Half))
-music.playTone(523, music.beat(BeatFraction.Half))
-music.playTone(587, music.beat(BeatFraction.Half))
-music.playTone(494, music.beat(BeatFraction.Half))
-music.playTone(784, music.beat(BeatFraction.Half))
-music.playTone(740, music.beat(BeatFraction.Half))
-music.playTone(587, music.beat(BeatFraction.Half))
-music.playTone(659, music.beat(BeatFraction.Half))
-music.playTone(392, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(523, music.beat(BeatFraction.Half))
-music.playTone(440, music.beat(BeatFraction.Half))
-music.playTone(262, music.beat(BeatFraction.Half))
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # # # # #
+        # . . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # . # #
+        # # . # #
+        # # . # #
+        . # # # .
+        `)
+    basic.pause(100)
+})
 basic.forever(function () {
 	
 })
